02c1b943bbf5756540091d076d8a6bf3d4123949
Bashitha Wijegunawardana <lbw@tiqri.com>
2018-07-17 19:16:42 +0530

    changes

---
+++ Web/MentoringPlatformApplication/Security.API/Controllers/ValueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Security.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Value")]
+    public class ValueController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
